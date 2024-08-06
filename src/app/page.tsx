3d18048b97201613913b732690e7d32f6ecdd17d
VicTech444 <Victor_verq@outlook.com>
2024-08-06 09:40:00 -0500

    remove testing cors function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,10 @@ import { Courses } from "@/components/Courses";
 import { TopHero } from "@/components/TopHero";
 
 export default function Home() {
-  const showTest = async () => {
-    let testingvar = await fetch('https://backend-seven-chi-92.vercel.app/');
-    let res = await testingvar.json()
-
-    console.log(res)
-  }
-
   return (
     <main className="py-20 flex flex-col gap-y-20">
       <TopHero header="Free courses for frontend and backend developers!" showDiscount description="Join with a thousand of students!"/>
       <Courses amountToShow={3}/>
-      <button onClick={showTest}>Hola mundo</button>
     </main>
   );
 }
