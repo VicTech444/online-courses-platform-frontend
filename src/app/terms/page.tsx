ae18939bbc6a08bc8dca2902340375d225456fd6
VicTech444 <Victor_verq@outlook.com>
2024-08-06 14:51:09 -0500

    create terms page

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,84 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-y-10 py-20">
+      <h1 className="text-left text-4xl font-bold">Term of service</h1>
+      <section className="flex flex-col gap-y-3">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ut?
+          Earum inventore tempore nobis aliquid suscipit, ipsa architecto
+          quaerat quo eos iure reiciendis maxime atque voluptates rerum.
+          Officiis, alias quos.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+          perspiciatis id odio saepe esse mollitia tempora, quo obcaecati,
+          nesciunt adipisci et sequi optio deserunt fugit a fuga aspernatur
+          voluptatum pariatur.
+        </p>
+      </section>
+      <section className="flex flex-col gap-y-3">
+        <h2 className="text-left text-2xl font-bold">Use of information</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+          ullam. Laborum iusto aperiam expedita ipsam unde, placeat odit,
+          dolorem fugit soluta reiciendis exercitationem excepturi explicabo ex
+          porro accusamus. Provident, dicta!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          nobis suscipit alias corporis delectus perferendis velit modi nulla
+          provident tempore neque dicta accusantium quas eius, eum ipsa, facere
+          magni facilis?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+          iusto, accusantium aliquam numquam id nihil adipisci quasi repellat
+          laudantium! Facere quae tempora minima eum nam impedit veritatis
+          officiis odit eaque? Reiciendis perspiciatis corporis eos at eum sunt
+          doloremque architecto, exercitationem quasi deserunt. Id in est quo
+          ratione, molestiae dolores a sunt commodi, sit rem quam odio fugit
+          amet! Nihil, fugit! Quibusdam facilis similique voluptates unde.
+          Corporis est, fugiat magnam ipsum reprehenderit deserunt odio
+          laudantium cupiditate aut! Ad possimus laborum, quia accusamus,
+          consectetur delectus aut, nam nemo voluptatum dolor veritatis earum.
+        </p>
+      </section>
+      <section className="flex flex-col gap-y-3">
+        <h2 className="text-left text-2xl font-bold">
+          Security of the information
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos odio
+          natus odit asperiores rem error hic quidem sequi reprehenderit aliquid
+          omnis quae porro culpa praesentium, veniam quas magnam. Repellat. Eius
+          autem ducimus aliquid ipsa omnis non eum quis suscipit consectetur
+          totam dicta adipisci dolores tempora in at saepe veniam quasi, facilis
+          sed commodi? Ut corporis nobis explicabo in consectetur.
+        </p>
+      </section>
+      <section className="flex flex-col gap-y-3">
+        <h2 className="text-left text-2xl font-bold">Link to other websites</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+          debitis provident eligendi, magnam sit nisi consequatur voluptas,
+          repellat fugit reprehenderit placeat suscipit, optio ipsam deserunt.
+          Corporis odit cum laborum nihil. Mollitia recusandae eius odio
+          corporis, dolor vero sint officia, fuga cum, sequi optio error
+          dolorum! Pariatur numquam dolorem impedit eos perferendis beatae
+          corrupti est sit dicta exercitationem, necessitatibus tempora esse?
+        </p>
+      </section>
+      <section className="flex flex-col gap-y-3">
+        <h2 className="text-left text-2xl font-bold">
+          Changes in privacy policy
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex atque
+          quidem porro! Consequuntur distinctio enim est voluptatem cumque ad,
+          at, necessitatibus quasi tempora sit quisquam natus voluptatibus modi
+          ipsum.
+        </p>
+      </section>
+    </main>
+  );
+}
