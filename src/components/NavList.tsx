e2c875d29ef82329f7cc139ffe7df3570445d9e9
VicTech444 <Victor_verq@outlook.com>
2024-08-06 15:11:54 -0500

    change spanish title to english

--- src/components/NavList.tsx
+++ src/components/NavList.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export const NavList = () => {
   return (
-    <ul className="flex gap-x-4">
+    <ul className="flex gap-x-6">
       <li>
         <Link href={"/"}>Home</Link>
       </li>
       <li>
       <Link href={"/all-courses/"}>Courses</Link>
       </li>
-      <Link href={'/log/'}>Iniciar Sesion</Link>
+      <Link href={'/log/'}>Log In</Link>
     </ul>
   );
 };
