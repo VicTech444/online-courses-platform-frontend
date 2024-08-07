f09840a078b730e8b2f2dc0b4d8425087eee2022
VicTech444 <Victor_verq@outlook.com>
2024-08-06 23:34:10 -0500

    create component to render the courses

---
+++ src/components/CoursesList.tsx
@@ -0,0 +1,35 @@
+import { CoursesProps } from "@/interfaces/interfaces";
+import { UseQueryResult } from "@tanstack/react-query";
+import Image from "next/image";
+
+export const CoursesList = ({queryResponse}: {queryResponse: UseQueryResult<CoursesProps, Error>}) => {
+    
+    if (queryResponse.isFetching){
+        return (
+            <div>Loading Content...</div>
+        )
+    }
+
+    if (queryResponse.error) {
+        return (
+            <div>Error while loading the content, try again later</div>
+        )
+    }
+
+    return (
+        <div className="grid min-[1199px]:grid-cols-3">
+            {
+                queryResponse.data?.courses.map(course => (
+                    <div className="px-4 pb-6 flex flex-col">
+                         <Image src={course.image} alt="Sample alt for images (all alts are the same)" className="object-cover aspect-video" width={500} height={320}/>
+                         <div className="p-4 bg-black text-white flex flex-col gap-y-3">
+                            <h3 className="text-[20px] font-bold">{course.title}</h3>
+                            <p className="text-base font-light">{course.description}</p>
+                            <span className="text-[#e8d2ff] mt-2 text-xl font-bold">For Free!</span>
+                         </div>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
