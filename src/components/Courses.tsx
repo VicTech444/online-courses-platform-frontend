ef7a040dcdd1219cc0ad1b101a2ade6ba185bcd8
VicTech444 <Victor_verq@outlook.com>
2024-08-06 23:33:45 -0500

    add query call response, add component to render the courses

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,16 +1,23 @@
+"use client"
+
 import { FC } from "react";
+import { coursesCallReactQuery } from "@/react-query/callCourses";
+import { CoursesList } from "./CoursesList";
 
 interface Props {
-  amountToShow: number;
+  amountToShow?: number;
 }
 
 export const Courses: FC<Props> = ({ amountToShow }) => {
+  let { queryResponse } = coursesCallReactQuery(amountToShow);
+  
   return (
     <section className="flex flex-col gap-y-10">
       <div className="mx-auto text-center">
         <h2 className="text-3xl font-bold">Courses</h2>
         <h3 className="text-xl">List of all courses on the platform</h3>
       </div>
+      <CoursesList queryResponse={queryResponse}/>
     </section>
   );
 };
