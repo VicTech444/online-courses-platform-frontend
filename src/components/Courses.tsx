00a38cfc42caeb7e069b02a7b773b4d63fbec6fc
VicTech444 <Victor_verq@outlook.com>
2024-08-06 23:42:52 -0500

    add border radius for project card

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -13,7 +13,7 @@ export const Courses: FC<Props> = ({ amountToShow }) => {
   
   return (
     <section className="flex flex-col gap-y-10">
-      <div className="mx-auto text-center">
+      <div className="mx-auto text-center mb-3">
         <h2 className="text-3xl font-bold">Courses</h2>
         <h3 className="text-xl">List of all courses on the platform</h3>
       </div>
--- src/components/CoursesList.tsx
+++ src/components/CoursesList.tsx
@@ -21,8 +21,8 @@ export const CoursesList = ({queryResponse}: {queryResponse: UseQueryResult<Cour
             {
                 queryResponse.data?.courses.map(course => (
                     <div className="px-4 pb-6 flex flex-col" key={course._id}>
-                         <Image src={course.image} alt="Sample alt for images (all alts are the same)" className="object-cover aspect-video" width={500} height={320}/>
-                         <div className="p-4 bg-black text-white flex flex-col gap-y-3">
+                         <Image src={course.image} alt="Sample alt for images (all alts are the same)" className="object-cover aspect-video rounded-t-md" width={500} height={320}/>
+                         <div className="p-4 bg-black text-white flex flex-col gap-y-3 rounded-b-md">
                             <h3 className="text-[20px] font-bold">{course.title}</h3>
                             <p className="text-base font-light">{course.description}</p>
                             <span className="text-[#e8d2ff] mt-2 text-xl font-bold">For Free!</span>
