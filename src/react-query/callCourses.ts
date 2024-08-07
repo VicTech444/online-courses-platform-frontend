9f204e487d4858fa9d474882fdb1cedca5bf8514
VicTech444 <Victor_verq@outlook.com>
2024-08-06 23:29:32 -0500

    improve react query request

--- src/react-query/callCourses.ts
+++ src/react-query/callCourses.ts
@@ -2,18 +2,19 @@ import { mongoInstance } from "@/axios/baseUrl";
 import { CoursesProps } from "@/interfaces/interfaces";
 import { useQuery } from "@tanstack/react-query";
 
-const coursesRequest = async (amountOfCourses: number | undefined) => {
+const coursesRequest = async (amountOfCourses: number | undefined): Promise<CoursesProps> => {
     try {
-        if (!amountOfCourses) {
-            let res = await mongoInstance.get('/courses/showAllCourses/');
-            if (res.status >= 200 || res.status <= 299) {
-                return res.data as CoursesProps;
-            } else {
-                throw new Error('An error ocurred while fetching the data')
-            }
+        if (amountOfCourses) {
+
         }
 
-        return 'Hello world'
+        let res = await mongoInstance.get('/courses/showAllCourses/');
+
+        if (res.status >= 200 || res.status <= 299) {
+            return res.data as CoursesProps;
+        } else {
+            throw new Error('An error ocurred while fetching the data')
+        }
     } catch (error: any) {
         throw new Error(error)
     }
