99d64b9eeb3b01a0a660136401db58af38203602
VicTech444 <Victor_verq@outlook.com>
2024-08-23 00:06:43 -0500

    testing

--- src/axios/baseUrl.ts
+++ src/axios/baseUrl.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 // https://backend-pi-orcin.vercel.app
+// http://localhost:3001
 export const mongoInstance = axios.create({
     baseURL: "https://backend-pi-orcin.vercel.app",
     withCredentials: true
