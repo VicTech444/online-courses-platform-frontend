994ae939ab9ee5db042e3d01b4aecfe55512bf28
VicTech444 <Victor_verq@outlook.com>
2024-08-22 19:57:18 -0500

    add credentials options to recieve cookies

--- src/axios/baseUrl.ts
+++ src/axios/baseUrl.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 // https://backend-pi-orcin.vercel.app
 export const mongoInstance = axios.create({
-    baseURL: "https://backend-pi-orcin.vercel.app"
+    baseURL: "https://backend-pi-orcin.vercel.app",
+    withCredentials: true
 })
